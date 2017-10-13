abde30d1f5d0b9bfe52e838d563aa06792030f55
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-12 21:05:38 -0400

    Create several files needed for upcoming UI components

---
+++ src/main/java/com/gitrekt/resort/controller/GuestHomeScreenController.java
@@ -0,0 +1,20 @@
+package com.gitrekt.resort.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class for the guest home screen.
+ */
+public class GuestHomeScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+}
