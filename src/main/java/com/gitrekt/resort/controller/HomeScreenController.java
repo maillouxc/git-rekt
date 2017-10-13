841e7d406e0241cd9ca520d50181a72b3f3cdb39
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-13 18:56:34 -0400

    Create prototype controller class for home screen

--- src/main/java/com/gitrekt/resort/controller/HomeScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/HomeScreenController.java
@@ -1,10 +1,16 @@
 package com.gitrekt.resort.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * The JavaFX controller class for the home screen.
@@ -26,15 +32,44 @@ public class HomeScreenController implements Initializable {
     }
     
     /**
-     * Trigger the pop-up dialog 
+     * Displays the pop-up dialog which prompts staff members to log in to the 
+     * system.
+     * 
+     * @throws IOException
      */
     @FXML
-    protected void onStaffModeButtonClicked() {
-        // TODO: Implement.
+    protected void onStaffModeButtonClicked() throws IOException {
+        Stage staffLoginDialogStage = new Stage();
+        Parent staffLoginDialogRoot = FXMLLoader.load(
+                getClass().getResource("/fxml/StaffLoginDialog.fxml")
+        );
+        Scene staffLoginDialog = new Scene(staffLoginDialogRoot);
+        
+        staffLoginDialogStage.setScene(staffLoginDialog);
+        staffLoginDialogStage.initModality(Modality.APPLICATION_MODAL);
+        staffLoginDialogStage.initOwner(staffModeButton.getScene().getWindow());
+        staffLoginDialogStage.setResizable(false);
+        staffLoginDialogStage.setTitle("Authentication Required");
+        staffLoginDialogStage.centerOnScreen();
+        
+        staffLoginDialogStage.show();
     }
     
+    /**
+     * Displays the guest home screen containing the various main use-case
+     * type actions that guests can perform within the software.
+     * 
+     * @throws IOException 
+     */
     @FXML
-    protected void onGuestModeButtonClicked() {
-        // TODO: Implement.
+    protected void onGuestModeButtonClicked() throws IOException {
+        Stage mainStage = (Stage) guestModeButton.getScene().getWindow();
+        Parent guestHomeScreenRoot = FXMLLoader.load(
+                getClass().getResource("/fxml/GuestHomeScreen.fxml")
+        );
+        
+        Scene guestHomeScreen = new Scene(guestHomeScreenRoot);
+        mainStage.setScene(guestHomeScreen);
+        mainStage.centerOnScreen();
     }
 }
