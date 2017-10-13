f19bd7ff271fe0c1f5491a40ecb1b759513d73e9
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-12 21:09:19 -0400

    Add files for leave feedback screen

---
+++ src/main/java/com/gitrekt/resort/controller/LeaveFeedbackScreenController.java
@@ -0,0 +1,20 @@
+package com.gitrekt.resort.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class for the leave feedback screen.
+ */
+public class LeaveFeedbackScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+}
