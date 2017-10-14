5f9f50422612d2684de988afd52bb7a430a0d613
jfleurent <jeffreyfleurent@gmail.com>
2017-10-14 14:22:22 -0400

    added a feedback 6 hours in

--- src/main/java/com/gitrekt/resort/controller/LeaveFeedbackScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/LeaveFeedbackScreenController.java
@@ -2,19 +2,42 @@ package com.gitrekt.resort.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class for the leave feedback screen.
  */
 public class LeaveFeedbackScreenController implements Initializable {
 
+
+     @FXML 
+     private javafx.scene.control.Button submitButton;
+    
     /**
-     * Initializes the controller class.
+     * Closes the window and saves the user's feedback to a database
+     * @param event 
      */
+     
+    @FXML
+    private void onSubmitButtonClicked(ActionEvent event) {
+        
+         Stage stage = (Stage) submitButton.getScene().getWindow();
+          //TODO Add feedback to database
+          stage.close();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
+    }
     
-}
+
+    
+    
+
+
+    
