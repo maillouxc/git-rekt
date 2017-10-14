9d55aa918207905e302b55e60757bb1d78b70711
Christopher Mailloux <mailloux.cl@gmail.com>
2017-10-14 14:45:45 -0400

    Fix missing semicolon

--- src/main/java/com/gitrekt/resort/controller/LeaveFeedbackScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/LeaveFeedbackScreenController.java
@@ -6,7 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
-import javafx.scene.control.Button
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class for the leave feedback screen.
