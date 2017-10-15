e2a24d84a1c374bb5fc1e0c0bfcd932afcc9d4f9
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-15 14:02:08 -0400

    Implement back button logic on browse rooms screen

--- src/main/java/com/gitrekt/resort/controller/BrowseRoomsScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/BrowseRoomsScreenController.java
@@ -1,14 +1,24 @@
 package com.gitrekt.resort.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class for the browse rooms screen.
  */
 public class BrowseRoomsScreenController implements Initializable {
 
+    @FXML
+    private Button backButton;
+    
     /**
      * Initializes the controller class.
      */
@@ -16,5 +26,22 @@ public class BrowseRoomsScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    /**
+     * Closes the browse-rooms screen and returns the application to the guest
+     * home screen.
+     * 
+     * @throws IOException
+     */
+    @FXML
+    protected void onBackButtonClicked() throws IOException {
+        Stage mainStage = (Stage) backButton.getScene().getWindow();
+        Parent guestHomeScreenRoot = FXMLLoader.load(
+                getClass().getResource("/fxml/GuestHomeScreen.fxml")
+        );
+        Scene guestHomeScreen = new Scene(guestHomeScreenRoot);
+        
+        mainStage.setScene(guestHomeScreen);
+    }
     
 }
