219ac7d1b13254547e843b15051ff8b2e0c2a018
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-16 01:01:49 -0400

    Messy Implemenation of Browse Rooms Screen
    Needs major revision before any kind of merge, but it's 1am and I called off work to figure this out.

--- src/main/java/com/gitrekt/resort/controller/BrowseRoomsListItemController.java
+++ src/main/java/com/gitrekt/resort/controller/BrowseRoomsListItemController.java
@@ -2,57 +2,93 @@ package com.gitrekt.resort.controller;
 
 import com.gitrekt.resort.model.RoomSearchResult;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.scene.image.ImageView;
 
 /**
  * TODO: Document.
  */
-public class BrowseRoomsListItemController extends ListCell<RoomSearchResult>{
+public class BrowseRoomsListItemController implements Initializable {
+    
+    @FXML
+    private Node root;
     
     @FXML
     private Label roomCategoryLabel;
     
     @FXML
-    private Label roomNumberLabel;
+    private Label bedsInfoLabel;
     
     @FXML
     private Label roomPriceLabel;
     
     @FXML
-    private ImageView roomCategoryPicture;
+    private ImageView roomThumbnailView;
     
-    private FXMLLoader fxmlLoader;
+    @FXML
+    private Label roomDescriptionLabel;
     
-    @Override
-    protected void updateItem(RoomSearchResult roomData, boolean empty) {
-        super.updateItem(roomData, empty);
-        
-        if(empty || roomData == null) {
-            // TODO: Handle
-        } else {
-            if(fxmlLoader == null) {
-                fxmlLoader = new FXMLLoader(
-                    getClass().getResource("/fxml/BrowseRoomsListItem.fxml")
-                );
-                
-                try {
-                    fxmlLoader.load();
-                } catch (IOException e) {
-                    // TODO: Handle
-                }
-            }
-            
-            // TODO: Updata cell data.
-            
-            roomCategoryLabel.setText(roomData.getRoomCategory().getName());
-            roomCategoryPicture.setImage(roomData.getRoomCategory().getImage());
-            roomNumberLabel.setText(roomData.getRoomNumber());
-            roomPriceLabel.setText(roomData.getRoomPrice());
+    public BrowseRoomsListItemController() {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/fxml/BrowseRoomsListItem.fxml")
+        );
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
         }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    public void setData(RoomSearchResult roomData) {
+        // TODO: Remove
+        System.out.println("here");
+        
+        //roomCategoryLabel.setText(roomData.getRoomCategory().getName());
+        roomThumbnailView.setImage(roomData.getRoomCategory().getImage());
+    
+        // TODO: We should handle currency more flexibly at some point.
+        String priceString = "$" + roomData.getRoomPrice() + " / night";
+                    
+        roomPriceLabel.setText(priceString);
+        roomDescriptionLabel.setText(
+            roomData.getRoomCategory().getDescription()
+        );
+        bedsInfoLabel.setText(roomData.getRoomCategory().getBedsInfo());
+    }
+    
+    public Node getView() {
+        return root;
+    }
+    
+    @FXML
+    protected void onAddToBookingButtonClicked() {
+        // TODO
+    }
+    
+    @FXML
+    protected void onMoreInfoButtonClicked() {
+        // TODO
+    }
+    
+    @FXML
+    protected void onRoomThumbnailClicked() {
+        // TODO
     }
     
 }
--- src/main/java/com/gitrekt/resort/controller/BrowseRoomsScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/BrowseRoomsScreenController.java
@@ -1,14 +1,22 @@
 package com.gitrekt.resort.controller;
 
+import com.gitrekt.resort.model.RoomCategory;
+import com.gitrekt.resort.model.RoomSearchResult;
+import com.gitrekt.resort.view.AvailableRoomListItem;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,13 +27,51 @@ public class BrowseRoomsScreenController implements Initializable {
     @FXML
     private Button backButton;
     
+    @FXML
+    private ListView<RoomSearchResult> roomsListView;
+    
+    @FXML
+    private Button findAvailableRoomsButton;
+    
+    private ObservableList<RoomSearchResult> roomSearchResults;
+    
+    public BrowseRoomsScreenController() {
+        roomSearchResults = FXCollections.observableArrayList();
+    }
+    
     /**
      * Initializes the controller class.
+     * 
+     * Don't touch. Magic.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        roomsListView.setItems(roomSearchResults);
+        roomsListView.setCellFactory(param -> new AvailableRoomListItem() {
+            {
+                prefWidthProperty().bind(roomsListView.widthProperty());
+            }
+        });
+
+    }
+    
+    public void addResult(RoomSearchResult result) {
+        if(!roomSearchResults.contains(result)) {
+            roomSearchResults.add(result);
+            sortResultsByPrice();
+        }
+    }
+    
+    public void removeResult(RoomSearchResult result) {
+        roomSearchResults.remove(result);
+    }
+    
+    private void sortResultsByPrice() {
+        roomSearchResults.sort(
+            (RoomSearchResult r1, RoomSearchResult r2) -> 
+                r1.getRoomPrice().compareTo(r2.getRoomPrice())
+        );
+    }
     
     /**
      * Closes the browse-rooms screen and returns the application to the guest
@@ -39,9 +85,27 @@ public class BrowseRoomsScreenController implements Initializable {
         Parent guestHomeScreenRoot = FXMLLoader.load(
                 getClass().getResource("/fxml/GuestHomeScreen.fxml")
         );
+        
         Scene guestHomeScreen = new Scene(guestHomeScreenRoot);
         
         mainStage.setScene(guestHomeScreen);
+    }
+    
+    @FXML
+    protected void onFindAvailableRoomsButtonClicked() {
+        System.out.println("here");
+        
+        Image i = new Image("/images/temporary_hotel_room_image_placeholder.jpg");
+        RoomSearchResult r4 = new RoomSearchResult("512", new BigDecimal("2419"), 
+            new RoomCategory(
+                "Basic Family Suite", 
+                "This room features a 40-inch flat-screen TV with high-definition channels. An iPod docking station and coffee-making facilities are also provided.\n" + "\n" + "Room Facilities: Safe, Air conditioning, Iron, Heating, Carpeted, Interconnecting room(s) available, Hairdryer, Free toiletries, Toilet, Bathroom, Bathtub or shower, Telephone, Radio, Cable channels, Flat-screen TV, Refrigerator, Alarm clock",
+                i,
+                "2 King, 2 Twin"
+            )
+        );
+        
+        this.addResult(r4);
     }
     
 }
