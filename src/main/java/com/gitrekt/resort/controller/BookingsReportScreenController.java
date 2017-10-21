7ecf1bda7ae88b61a61153fe7095df5998c9f45f
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-20 23:18:30 -0400

    More work on bookings-report-screen controller skeleton

--- src/main/java/com/gitrekt/resort/controller/BookingsReportScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/BookingsReportScreenController.java
@@ -2,9 +2,13 @@ package com.gitrekt.resort.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -30,13 +34,21 @@ public class BookingsReportScreenController implements Initializable {
     
     @FXML
     private Button backButton;
+
+    private XYChart.Series<String, Number> roomCategoriesDataSeries;
+    
+    private ObservableList<Data<String, Number>> roomCategoriesList;
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // TODO: Test using test data, then implement with real logic.
+        Data<String,Number> testData = new Data<String,Number>();
+        roomCategoriesList = FXCollections.observableArrayList();
+        roomCategoriesDataSeries = new XYChart.Series<>();
+        roomCategoriesDataSeries.setData(roomCategoriesList);
     }    
     
     @FXML
