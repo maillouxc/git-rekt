4e854876c6c753614c96e7e6c67e78246d0240ac
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-20 22:44:47 -0400

    Prototype bookings report screen

--- src/main/java/com/gitrekt/resort/controller/BookingsReportScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/BookingsReportScreenController.java
@@ -2,13 +2,35 @@ package com.gitrekt.resort.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class for the management bookings report screen.
  */
 public class BookingsReportScreenController implements Initializable {
 
+    @FXML
+    private StackedBarChart barChart;
+    
+    @FXML
+    private Label monthYearLabel;
+    
+    @FXML
+    private Button nextMonthButton;
+    
+    @FXML
+    private Button previousMonthButton;
+    
+    @FXML
+    private Button pickMonthButton;
+    
+    @FXML
+    private Button backButton;
+    
     /**
      * Initializes the controller class.
      */
@@ -16,5 +38,25 @@ public class BookingsReportScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    private void onPickMonthButtonClicked() {
+        // TODO
+    }
+    
+    @FXML
+    private void onNextMonthButtonClicked() {
+        // TODO
+    }
+    
+    @FXML
+    private void onPreviousMonthButtonClicked() {
+        // TODO
+    }
+    
+    @FXML
+    private void onBackButtonClicked() {
+        // TODO
+    }
     
 }
