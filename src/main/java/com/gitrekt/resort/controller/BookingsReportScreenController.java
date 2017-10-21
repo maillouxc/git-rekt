0ddafeb30542abd19d85375095f72574c6b866be
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-20 21:13:49 -0400

    Create skeleton files for bookings report screen

---
+++ src/main/java/com/gitrekt/resort/controller/BookingsReportScreenController.java
@@ -0,0 +1,20 @@
+package com.gitrekt.resort.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class for the management bookings report screen.
+ */
+public class BookingsReportScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
