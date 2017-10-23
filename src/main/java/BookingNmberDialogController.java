1d2c614650d3677669ac6a32bdbadd93b2bc3887
jeffrey <jeffreyfleurent@outlook.com>
2017-10-23 11:48:46 -0400

    Create layout for booking number dialog

---
+++ src/main/java/BookingNmberDialogController.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ */
+public class BookingNmberDialogController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
