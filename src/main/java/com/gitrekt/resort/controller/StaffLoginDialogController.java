358b9183c4d0877980af670eefbac0ae8cfb5ff9
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-23 17:45:49 -0400

    Fix buttons on staff login dialog

--- src/main/java/com/gitrekt/resort/controller/StaffLoginDialogController.java
+++ src/main/java/com/gitrekt/resort/controller/StaffLoginDialogController.java
@@ -9,8 +9,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
     
 /**
  * FXML Controller class for the staff login dialog.
@@ -22,6 +23,13 @@ public class StaffLoginDialogController implements Initializable {
     
     @FXML
     private Button loginButton;
+    
+    @FXML
+    private PasswordField passwordField;
+    
+    @FXML
+    private TextField employeeIdField;
+    
     /**
      * Initializes the controller class.
      */
@@ -30,23 +38,34 @@ public class StaffLoginDialogController implements Initializable {
         // TODO
     }
     
-    @FXML
-    protected void onCancelClicked() throws IOException {
-        Stage closeStaffWindow = (Stage) cancelButton.getScene().getWindow();
-        closeStaffWindow.close();
+    public void onCancelButtonClicked() {
+        Stage dialogStage = (Stage) cancelButton.getScene().getWindow();
+        dialogStage.close();
     }
     
-    @FXML
-    protected void onLoginClicked() throws IOException {
-        Stage staffLoginStage = (Stage) loginButton.getScene().getWindow();
-        Parent staffHomeScreenRoot = FXMLLoader.load(
-            getClass().getResource("/fxml/StaffHomeScreen.fxml")
-        );
-        //Scene staffHomeScreenDialog = new Scene(staffHomeScreenRoot);
-        //staffLoginStage.setScene(staffHomeScreenDialog);
+    public void onLoginButtonClicked() throws IOException {
+        boolean validCredentials = areEmployeeCredentialsValid();
+        if(validCredentials) {
+            Stage dialogStage = (Stage) loginButton.getScene().getWindow();
+            Stage mainStage = (Stage) dialogStage.getOwner();
+            Parent staffHomeScreenRoot = FXMLLoader.load(
+                getClass().getResource("/fxml/StaffHomeScreen.fxml")
+            );
+            Scene staffHomeScreenScene = new Scene(staffHomeScreenRoot);
+            mainStage.setScene(staffHomeScreenScene);
+            dialogStage.close();
+        } else {
+            // TODO
+        }
+    }
+    
+    private boolean areEmployeeCredentialsValid() {
+        // Read the data from the fields to validate it
+        String employeeIdEntered = employeeIdField.getText();
+        String passwordEntered = passwordField.getText();
         
-        
-        
+        // For now, just return true.
+        return true;
     }
     
 }
