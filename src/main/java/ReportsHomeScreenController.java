03ae887c69a276698868a5b5e6567f2b80ae8a31
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-24 13:57:25 -0400

    Skeleton controller for staff report screen

--- src/main/java/ReportsHomeScreenController.java
+++ src/main/java/ReportsHomeScreenController.java
@@ -1,12 +1,23 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class for reports home screen.
  */
 public class ReportsHomeScreenController implements Initializable {
 
+    @FXML
+    private Button backButton;
+    
+    @FXML
+    private Button bookingPercentagesReportButton;
+    
+    @FXML
+    private Button incomeReportButton;
+    
     /**
      * Initializes the controller class.
      */
@@ -14,5 +25,17 @@ public class ReportsHomeScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void onBackButtonClicke() {
+        // TODO
+    }
+    
+    public void onBookingPercentagsButtonClicked() {
+        // TODO
+    }
+    
+    public void onIncomeReportButttonClicked() {
+        // TODO
+    }
     
 }
