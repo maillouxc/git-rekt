78330fbd68cf3d2a0baf922729e23d959a435881
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-24 19:50:08 -0400

    Skeleton controller for view booking screen

---
+++ src/main/java/com/gitrekt/resort/controller/BookingDetailsScreenController.java
@@ -0,0 +1,62 @@
+package com.gitrekt.resort.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class for the booking details screen.
+ */
+public class BookingDetailsScreenController implements Initializable {
+
+    @FXML
+    private Button backButton;
+    
+    @FXML
+    private Button cancelBookingButton;
+    
+    @FXML
+    private Button viewBillButton;
+    
+    // TODO fix rawtype
+    @FXML
+    private TableView bookedRoomsTableView;
+    
+    // TODO fix rawtype
+    @FXML
+    private TableView bookedPackagesTableView;
+    
+    @FXML
+    private Label guestNameLabel;
+    
+    @FXML
+    private Label checkInDateLabel;
+    
+    @FXML
+    private Label checkOutDateLabel;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    public void onBackButtonClicked() {
+        // TODO
+    }
+    
+    public void onViewBillButtonClicked() {
+        // TODO
+    }
+    
+    public void onCancelBookingButtonClicked() {
+        // TODO
+    }
+    
+}
