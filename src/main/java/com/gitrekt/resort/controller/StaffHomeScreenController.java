965bde15e839382fa7841cfecd231ca43593fa2f
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-24 14:05:45 -0400

    Display view reports screen on button click

--- src/main/java/com/gitrekt/resort/controller/StaffHomeScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/StaffHomeScreenController.java
@@ -1,14 +1,24 @@
 package com.gitrekt.resort.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class for the staff home screen.
  */
 public class StaffHomeScreenController implements Initializable {
 
+    @FXML
+    private Button viewReportsButton;
+    
     /**
      * Initializes the controller class.
      */
@@ -16,5 +26,14 @@ public class StaffHomeScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void onViewReportsButtonClicked() throws IOException {
+        Stage mainStage = (Stage) viewReportsButton.getScene().getWindow();
+        Parent viewReportsScreenRoot = FXMLLoader.load(
+            getClass().getResource("/fxml/ViewReportsScreenController.fxml")
+        );
+        Scene viewReportsScreen = new Scene(viewReportsScreenRoot);
+        mainStage.setScene(viewReportsScreen);
+    }
     
 }
