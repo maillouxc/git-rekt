f28c05e01d027fa8a4e6b693defd156a8849cd09
DevonJerothe <devonjerothe@gmail.com>
2017-10-25 16:34:24 -0400

    PlaceBookingScreenController
    *Add ObservableList of all countries in ISO and input them (or at least try) into countryPicker comboBox

--- src/main/java/com/gitrekt/resort/controller/PlaceBookingScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/PlaceBookingScreenController.java
@@ -1,7 +1,13 @@
 package com.gitrekt.resort.controller;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -59,9 +65,11 @@ public class PlaceBookingScreenController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
         // Hide the state picker until we know if the user is from the US
         statePicker.setVisible(false);
     }
+
     
     @FXML
     protected void onBackButtonClicked() {
@@ -76,5 +84,15 @@ public class PlaceBookingScreenController implements Initializable {
     @FXML
     protected void onCountryPicked() {
         // TODO: If country is US, show state picker.
+        ObservableList<String> countries = FXCollections.observableArrayList();
+        //ComboBox<String> countryPicker = new ComboBox<>(countries);
+        Locale[] locales = Locale.getAvailableLocales();
+        for(Locale locale : locales){
+            String name = locale.getDisplayCountry();
+            if(!countries.contains(name)){
+                countries.add(name);
+            }
+        }
+        countryPicker.setItems(countries);
     }
 }
