ecd781e56c7841ec2a5f7ef8756e1331df88e252
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-25 10:46:53 -0400

    Implement back button on staff-accounts-screeen

--- src/main/java/com/gitrekt/resort/controller/StaffAccountsScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/StaffAccountsScreenController.java
@@ -1,14 +1,24 @@
 package com.gitrekt.resort.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class.
  */
 public class StaffAccountsScreenController implements Initializable {
 
+    @FXML
+    private Button backButton;
+    
     /**
      * Initializes the controller class.
      */
@@ -16,5 +26,14 @@ public class StaffAccountsScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void onBackButtonClicked() throws IOException {
+        Stage mainStage = (Stage) backButton.getScene().getWindow();
+        Parent staffHomeScreenRoot = FXMLLoader.load(
+            getClass().getResource("/fxml/StaffHomeScreen.fxml")
+        );
+        Scene staffHomeScreen = new Scene(staffHomeScreenRoot);
+        mainStage.setScene(staffHomeScreen);
+    }
     
 }
