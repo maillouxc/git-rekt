d12f3fecc7ede359809877da954802dc651d14ea
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-25 10:23:38 -0400

    Create files for staff accounts screen

---
+++ src/main/java/com/gitrekt/resort/controller/StaffAccountsScreenController.java
@@ -0,0 +1,20 @@
+package com.gitrekt.resort.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class.
+ */
+public class StaffAccountsScreenController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
