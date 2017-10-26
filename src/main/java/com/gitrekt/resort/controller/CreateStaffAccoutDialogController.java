6c168e718cd9d943ff4ea41d7657cc197b800658
EMG <emeruvia@gmail.com>
2017-10-26 12:01:12 -0400

    Created staff account dialog fxml and dialog, no controller implementations yet

---
+++ src/main/java/com/gitrekt/resort/controller/CreateStaffAccoutDialogController.java
@@ -0,0 +1,15 @@
+package com.gitrekt.resort.controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author EMG
+ */
+public class CreateStaffAccoutDialogController {
+    
+}
