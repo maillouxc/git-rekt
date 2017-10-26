98082185faa7d16145940de4b0de734e120ad09b
jeffrey <jeffreyfleurent@outlook.com>
2017-10-26 13:01:15 -0400

    Made FeedbackReportListItemScreen.fxml

--- src/main/java/com/gitrekt/resort/controller/FeedbackReportScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/FeedbackReportScreenController.java
@@ -1,14 +1,23 @@
 package com.gitrekt.resort.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * Handles the functionality of FeesbackReportScreen.fsml
  */
 public class FeedbackReportScreenController implements Initializable {
 
+    @FXML
+    private Button backButton;
     /**
      * Initializes the controller class.
      */
@@ -16,5 +25,13 @@ public class FeedbackReportScreenController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+    public void onBackButtonClicked() throws IOException {
+        Stage mainStage = (Stage) backButton.getScene().getWindow();
+        Parent staffHomeScreenRoot = FXMLLoader.load(
+            getClass().getResource("/fxml/ReportsHomeScreen.fxml")
+        );
+        Scene staffHomeScreen = new Scene(staffHomeScreenRoot);
+        mainStage.centerOnScreen();
+        mainStage.setScene(staffHomeScreen);
+    }
 }
