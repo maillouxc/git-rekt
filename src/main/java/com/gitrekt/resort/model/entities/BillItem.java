59f400480d23646dc846d197fd02a9ed4f81235b
Chris Mailloux <mailloux.cl@gmail.com>
2017-10-29 14:50:24 -0400

    Add fields to Bill and BillItem classes

--- src/main/java/com/gitrekt/resort/model/entities/BillItem.java
+++ src/main/java/com/gitrekt/resort/model/entities/BillItem.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.gitrekt.resort.model.entities;
 
-/**
- *
- * @author jeffr
- */
 public class BillItem {
+    
+    private final String name;
+    
+    private final Long price;
+    
+    private int quantity;
+    
+    public BillItem(String name, Long price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Long getPrice() {
+        return price;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
     
 }
