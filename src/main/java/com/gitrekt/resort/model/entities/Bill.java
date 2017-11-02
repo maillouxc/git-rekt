87104dfd5a617364bebe04380c05772f1b131ca7
Chris Mailloux <mailloux.cl@gmail.com>
2017-11-02 16:52:16 -0400

    Add annotations to existing entities - fix schema issues

--- src/main/java/com/gitrekt/resort/model/entities/Bill.java
+++ src/main/java/com/gitrekt/resort/model/entities/Bill.java
@@ -2,9 +2,25 @@
 package com.gitrekt.resort.model.entities;
 
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "bills")
 public class Bill {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable(name = "bills_charges")
     private List<BillItem> charges;
     
     public Bill() {
