460eb07878961e8a3d83da46952d83e8cd55100f
jeffrey <jeffreyfleurent@outlook.com>
2017-11-04 09:45:59 -0400

    Add Guest, GuestService, MaillingAddress

--- src/main/java/com/gitrekt/resort/model/Guest.java
+++ src/main/java/com/gitrekt/resort/model/Guest.java
@@ -4,7 +4,52 @@ package com.gitrekt.resort.model;
  * TODO: Document
  */
 public class Guest {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+    private String mailingAddress;
+    private boolean isCheckedIn;
+
+    public Guest(Long id, String firstName, String lastName, String lasrName,
+            String emailAddress, String phoneNumber,
+            String mailingAddress, boolean isCheckedIn) {
+        this.emailAddress = emailAddress;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.mailingAddress = mailingAddress;
+        this.isCheckedIn = isCheckedIn;
+    }
+
+    public Long getId() {
+        return id;
+    }
     
-    // TODO: Implement.
+    public String getLastName(){
+        return lastName;
+    }
     
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public String getEmailAddress(){
+        return emailAddress;
+    }
+    
+    public String getMailingAddress(){
+        return mailingAddress;
+    }
+    
+    public String phoneNumber(){
+        return phoneNumber;
+    }
+    
+    public boolean isCheckedIn(){
+        return isCheckedIn;
+    }
 }
