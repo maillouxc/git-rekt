66d719318ce5a9f668900c599a478ecfea8e4a45
jeffrey <jeffreyfleurent@outlook.com>
2017-11-04 11:24:40 -0400

    Add setters to Guest

--- src/main/java/com/gitrekt/resort/model/entities/Guest.java
+++ src/main/java/com/gitrekt/resort/model/entities/Guest.java
@@ -4,49 +4,66 @@ package com.gitrekt.resort.model.entities;
  * TODO: Document
  */
 public class Guest {
-
-    private Long id;
+    
     private String firstName;
     private String lastName;
     private String emailAddress;
     private String phoneNumber;
     private MailingAddress mailingAddress;
-    private boolean isCheckedIn;
-
-    public Guest(Long id, String firstName, String lastName, String lasrName,
+    private boolean isCheckedIn = true;
+    
+    protected Guest(){
+    
+    }
+    
+    public Guest(String firstName, String lastName,
             String emailAddress, String phoneNumber,
-            MailingAddress mailingAddress, boolean isCheckedIn) {
+            MailingAddress mailingAddress) {
         this.emailAddress = emailAddress;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.id = id;
         this.phoneNumber = phoneNumber;
         this.mailingAddress = mailingAddress;
-        this.isCheckedIn = isCheckedIn;
-    }
-
-    public Long getId() {
-        return id;
     }
     
     public String getLastName(){
         return lastName;
     }
     
+    public void setLastName(String lastName){
+           this.lastName = lastName;
+    }
+    
     public String getFirstName(){
         return firstName;
+    }
+    
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
     }
     
     public String getEmailAddress(){
         return emailAddress;
     }
     
+    public void setEmailAddress(String emailingAddress){
+        this.emailAddress = emailingAddress;
+    }
+    
     public MailingAddress getMailingAddress(){
         return mailingAddress;
     }
     
-    public String phoneNumber(){
+    public void setMailingAddress(MailingAddress mailingAddress){
+        this.mailingAddress = mailingAddress;
+    }
+    
+    public String getphoneNumber(){
         return phoneNumber;
+    }
+    
+    public void setphoneNUmber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
     }
     
     public boolean isCheckedIn(){
