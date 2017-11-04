27e143e56ff84fcfb14569fd141415a4a9cbf3c0
jeffrey <jeffreyfleurent@outlook.com>
2017-11-04 10:10:08 -0400

    Made changes to Guest, MailingAddress, and GuestService

--- src/main/java/com/gitrekt/resort/model/MailingAddress.java
+++ src/main/java/com/gitrekt/resort/model/MailingAddress.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.gitrekt.resort.model;
 
-/**
- *
- * @author jeffr
- */
 public class MailingAddress {
     
+    private String addressLine1;
+    private String addressLine2;
+    private String postalCode;
+    //TODO Add state and country fields
+    
+    public MailingAddress(String addressLine1,String addressLine2,
+            String postalCode){
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.postalCode = postalCode;
+    }
+    
+    public String getAddressLine1(){
+        return addressLine1;
+    }
+    
+    public String getAddressLine2(){
+        return addressLine2;
+    }
+    
+    public String getPostalCode(){
+        return postalCode;
+    }
+    
+    //TODO Add getters for state and country
 }
