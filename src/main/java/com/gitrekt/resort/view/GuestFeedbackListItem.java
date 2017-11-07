dfdefb2c9b7e41b1f4f51bd5e4df9a9b3ff03ef6
Chris Mailloux <mailloux.cl@gmail.com>
2017-11-06 22:18:24 -0500

    Implement complete guest feedback report screen

--- src/main/java/com/gitrekt/resort/view/GuestFeedbackListItem.java
+++ src/main/java/com/gitrekt/resort/view/GuestFeedbackListItem.java
@@ -1,9 +1,9 @@
 package com.gitrekt.resort.view;
 
+import com.gitrekt.resort.controller.FeedbackReportScreenController;
 import com.gitrekt.resort.controller.GuestFeedbackListItemController;
 import com.gitrekt.resort.model.entities.GuestFeedback;
 import java.io.IOException;
-import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.ListCell;
@@ -16,8 +16,18 @@ public class GuestFeedbackListItem extends ListCell<GuestFeedback> {
     
     private final Node view;
     
-    public GuestFeedbackListItem() {
+    private FeedbackReportScreenController parentController;
+    
+    private GuestFeedback data;
+    
+    public GuestFeedbackListItem(
+            FeedbackReportScreenController parentController) {
         super();
+        
+        // This is a really quick and dirty way to do this.
+        // It's really tightly coupled but I don't have time right now to
+        // come up with a better design. It gets the job done.
+        this.parentController = parentController;
         
         try {
             fxmlLoader = new FXMLLoader(
@@ -32,9 +42,17 @@ public class GuestFeedbackListItem extends ListCell<GuestFeedback> {
         }
     }
     
+    /**
+     * Updates the contents of the list item that is displayed.
+     * 
+     * @param feedback The feedback data to display in the list item.
+     * @param empty Whether or not the list item is empty.
+     */
     @Override
     protected void updateItem(GuestFeedback feedback, boolean empty) {
         super.updateItem(feedback, empty);
+        
+        this.data = feedback;
         
         if(empty) {
             setGraphic(null);
@@ -45,7 +63,7 @@ public class GuestFeedbackListItem extends ListCell<GuestFeedback> {
     }
     
     public void onMarkedResolved() {
-        setGraphic(null);
+        parentController.hideItem(this.data);
     }
     
 }
