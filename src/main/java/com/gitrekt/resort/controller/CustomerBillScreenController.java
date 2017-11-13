d4b7aeddf580d7a188bcc0e5b44cfbf02d75c73e
Chris Mailloux <mailloux.cl@gmail.com>
2017-11-13 11:20:22 -0500

    Init booking number label on view bill screen

--- src/main/java/com/gitrekt/resort/controller/CustomerBillScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/CustomerBillScreenController.java
@@ -2,13 +2,13 @@ package com.gitrekt.resort.controller;
 
 import com.gitrekt.resort.model.entities.Bill;
 import com.gitrekt.resort.model.services.BillService;
+import com.gitrekt.resort.model.services.BookingService;
 import java.awt.print.PrinterException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -17,9 +17,6 @@ import javafx.scene.control.TableView;
  * FXML controller class for the customer bill view screen.
  */
 public class CustomerBillScreenController implements Initializable {
-
-    @FXML
-    private Button backButton;
     
     @FXML
     private Label billTotalText;
@@ -49,25 +46,39 @@ public class CustomerBillScreenController implements Initializable {
     @FXML
     private TableColumn priceColumn;
     
+    private Bill bill;
+    
+    private Long bookingNumber;
+    
+    private String customerName;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        String formattedPrice = String.format("%.2f", bill.getTotal());
+        billTotalText.setText(formattedPrice);
+    }
     
-    public void onBackButtonClicked() {
+    public void initializeData(Long bookingNumber) {
+        this.bookingNumber = bookingNumber;
+        bookingNumberText.setText(String.valueOf(bookingNumber));
+    }
+    
+    @FXML
+    private void onBackButtonClicked() {
         ScreenManager.getInstance().switchToScreen(
             "/fxml/BookingDetailsScreen.fxml"
         );
     }
     
-    public void onPrintBillButtonClicked() 
+    @FXML
+    private void onPrintBillButtonClicked() 
             throws IOException, PrinterException {
         // TODO: Replace with an actual bill, not an empty one.
         BillService billService = new BillService();
         billService.printBill(new Bill());
     }
-    
+
 }
