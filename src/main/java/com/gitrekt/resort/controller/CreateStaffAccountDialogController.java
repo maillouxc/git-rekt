c39f41fbf729f8483f8ed80d076687425026b5cd
Edgar Meruvia Garron <emeruvia@gmail.com>
2017-11-15 15:24:23 -0500

    Input for staff validation check should be working, have not implemented a check feature to see if the account already exists.

--- src/main/java/com/gitrekt/resort/controller/CreateStaffAccountDialogController.java
+++ src/main/java/com/gitrekt/resort/controller/CreateStaffAccountDialogController.java
@@ -1,13 +1,18 @@
 package com.gitrekt.resort.controller;
 
+import com.gitrekt.resort.model.services.PasswordValidatorUtil;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.passay.PasswordData;
+import org.passay.RuleResult;
+import org.passay.RuleResultDetail;
 
 /**
  * Controller class for the dialog in which managers create new staff accounts.
@@ -37,6 +42,30 @@ public class CreateStaffAccountDialogController implements Initializable {
     
     @FXML
     private TextField confirmPasswordField;
+    
+    @FXML
+    private Label firstNameLabel;
+    
+    @FXML
+    private Label lastNameLabel;
+    
+    @FXML
+    private Label employeeIdLabel;
+    
+    @FXML
+    private Label staffErrorLabel;
+
+    @FXML 
+    private Label errorLabel;
+    
+    @FXML
+    private Label lengthLabel;
+    
+    @FXML
+    private Label numAndSpecialCharLabel;
+    
+    @FXML
+    private Label mixedCaseLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -49,7 +78,107 @@ public class CreateStaffAccountDialogController implements Initializable {
     }
     
     public void onConfirmButtonClicked() {
-        //TODO
+        
+        String name = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        boolean isManager = managerCheckBox.isSelected();
+        long employeeId = Long.valueOf(employeeIdField.getText());
+        
+    }
+
+    private void onPasswordFieldUpdated() {
+        validatePasswords();
+    }
+    
+    private void onConfirmPasswordFieldUpdated() {
+        validatePasswords();
+    }
+    
+    private boolean validateStaffAccount() {
+        boolean result = true;
+        
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        boolean isManager = managerCheckBox.isSelected();
+        long employeeId = Long.valueOf(employeeIdField.getText());
+        
+        //Hide any already showing errors
+        staffErrorLabel.setVisible(false);
+        firstNameLabel.getStyleClass().remove("validationErrorText");
+        lastNameLabel.getStyleClass().remove("validationErrorText");
+        employeeIdLabel.getStyleClass().remove("validationErrorText");
+        
+        if (firstName.isEmpty() 
+                || lastName.isEmpty() 
+                || String.valueOf(employeeId).isEmpty()) {
+            staffErrorLabel.setVisible(true);
+            staffErrorLabel.setText("Fields cannot be empty");
+            result = false;
+        }
+
+        return result;
+    }
+    
+    
+    /**
+     * In addition to determining whether the passwords are valid, this method
+     * also handles the UI cues that let the user know what is wrong with their
+     * password.
+     * @return True if the passwords entered are valid
+     */
+    private boolean validatePasswords() {
+        boolean result = true;
+        
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+        
+        //hide any errors that are showing already
+        errorLabel.setVisible(false);
+        lengthLabel.getStyleClass().remove("validationErrorText");
+        numAndSpecialCharLabel.getStyleClass().remove("validationErrorText");
+        mixedCaseLabel.getStyleClass().remove("validationErrorText");
+        
+        //check if there is any text written in the passwords fields
+        if (password.isEmpty() || confirmPassword.isEmpty()) {
+            errorLabel.setVisible(true);
+            errorLabel.setText("Fields cannot be empty");
+            result =   false;
+        }
+        
+        //check if password matches confirmation password
+        if (!password.equals(confirmPassword)) {
+            errorLabel.setVisible(true);
+            errorLabel.setText("Passwords don't match");
+            result = false;
+        }
+        
+        RuleResult ruleResult = PasswordValidatorUtil.validator
+                .validate(new PasswordData(password));
+        
+        if (!ruleResult.isValid()) {
+            result = false;
+        }
+        
+        //Update the label requirements
+        for (RuleResultDetail d : ruleResult.getDetails()) {
+            String errorCode = d.getErrorCode();
+            
+            switch(errorCode) {
+                case "TOO_SHORT":
+                    lengthLabel.getStyleClass().add("validationErrorText");
+                    break;
+                case "INSUFFICIENT_LOWERCASE":
+                case "INSUFFICIENT_UPPERCASE":
+                    mixedCaseLabel.getStyleClass().add("validationErrorText");
+                    break;
+                case "INSUFFICIENT_DIGIT":
+                case "INSUFFICIENT_SPECIAL":
+                    numAndSpecialCharLabel.getStyleClass().add("validationErrorText");
+                    break;
+            }
+        }
+        
+        return result;
     }
     
 }
