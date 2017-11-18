77543f3e29af14eba2880b49a05c04f8cd3cac62
Chris Mailloux <mailloux.cl@gmail.com>
2017-11-17 23:42:21 -0500

    Implement cancel booking dialog

--- src/main/java/com/gitrekt/resort/controller/BookingDetailsScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/BookingDetailsScreenController.java
@@ -3,9 +3,13 @@ package com.gitrekt.resort.controller;
 import com.gitrekt.resort.model.entities.Booking;
 import com.gitrekt.resort.model.services.BookingService;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 
@@ -13,26 +17,26 @@ import javafx.scene.control.TableView;
  * FXML Controller class for the booking details screen.
  */
 public class BookingDetailsScreenController implements Initializable {
-    
+
     // TODO fix rawtype
     @FXML
     private TableView bookedRoomsTableView;
-    
+
     // TODO fix rawtype
     @FXML
     private TableView bookedPackagesTableView;
-    
+
     @FXML
     private Label guestNameLabel;
-    
+
     @FXML
     private Label checkInDateLabel;
-    
+
     @FXML
     private Label checkOutDateLabel;
-    
+
     private Booking booking;
-    
+
     /**
      * Initializes the controller class.
      */
@@ -42,14 +46,14 @@ public class BookingDetailsScreenController implements Initializable {
         BookingService bookingService = new BookingService();
         this.booking = bookingService.getBookingById(1L);
     }
-    
+
     @FXML
     private void onBackButtonClicked() {
         ScreenManager.getInstance().switchToScreen(
             "/fxml/GuestHomeScreen.fxml"
         );
     }
-    
+
     @FXML
     private void onViewBillButtonClicked() {
         Object temp = ScreenManager.getInstance().switchToScreen(
@@ -57,16 +61,36 @@ public class BookingDetailsScreenController implements Initializable {
         );
         CustomerBillScreenController controller;
         controller = (CustomerBillScreenController) temp;
-        
+
         // Fix LazyInitializationException by forcing initialization here.
         System.out.println(this.booking.toString());
-        
+
         controller.initializeData(this.booking);
     }
-    
+
+    /**
+     * Displays a confirmation dialog showing the price that will be charged as a cancellation fee.
+     */
     @FXML
     private void onCancelBookingButtonClicked() {
-        // TODO
+        double cancellationFee = BookingService.calcCancellationFee(this.booking);
+        String cancellationFeeString = String.format("$%.2f", cancellationFee);
+        // Show confirmation dialog
+        Alert confirmationDialog = new Alert(AlertType.CONFIRMATION);
+        confirmationDialog.setTitle("Confirm");
+        confirmationDialog.setHeaderText("Confirm Cancel Booking");
+        confirmationDialog.setContentText("You will be assessed a cancellation fee of "
+                + cancellationFeeString
+                + " - please confirm that you want to cancel this booking. "
+                + "This action cannot be undone.");
+
+        Optional<ButtonType> result = confirmationDialog.showAndWait();
+        if(result.get() == ButtonType.OK) {
+            // User chose OK
+            BookingService bookingService = new BookingService();
+            bookingService.cancelBooking(this.booking);
+            onBackButtonClicked();
+        }
     }
-    
+
 }
--- src/main/java/com/gitrekt/resort/model/services/BookingService.java
+++ src/main/java/com/gitrekt/resort/model/services/BookingService.java
@@ -28,11 +28,10 @@ public class BookingService {
 
     /**
      * Constructs a basic instance of BookingService.
-     * 
-     * Initializes an entityManager for the service, which is used to manage
-     * Hibernate entities for database persistence. This entityManager is later
-     * closed by the finalize method and so does not need to (nor can it be)
-     * explicitly closed.
+     *
+     * Initializes an entityManager for the service, which is used to manage Hibernate entities for
+     * database persistence. This entityManager is later closed by the finalize method and so does
+     * not need to (nor can it be) explicitly closed.
      */
     public BookingService() {
         this.entityManager = HibernateUtil.getEntityManager();
@@ -40,8 +39,8 @@ public class BookingService {
 
     /**
      * Takes care of closing the Hibernate entityManager for the class.
-     * 
-     * @throws Throwable 
+     *
+     * @throws Throwable
      */
     @Override
     public void finalize() throws Throwable {
@@ -51,15 +50,13 @@ public class BookingService {
 
     /**
      * Stores a new booking in the database.
-     * 
-     * Currently handles no other business logic besides this simple store 
-     * operation. This could change in the future before v1.0 release, before
-     * the service interface is locked down. 
-     * 
-     * Also sends a confirmation email to the user with the booking number for 
-     * future reference.
-     * 
-     * @param booking The booking object to persist to the data store. 
+     *
+     * Currently handles no other business logic besides this simple store operation. This could
+     * change in the future before v1.0 release, before the service interface is locked down.
+     *
+     * Also sends a confirmation email to the user with the booking number for future reference.
+     *
+     * @param booking The booking object to persist to the data store.
      */
     public void createBooking(Booking booking) {
         try {
@@ -75,23 +72,22 @@ public class BookingService {
     }
 
     /**
-     * @param id The unique id of the booking to return.
-     * 
+     * @param id The unique id of the booking to return from the database.
+     *
      * @return A booking with the provided id.
      */
     public Booking getBookingById(Long id) {
-        Booking booking = entityManager.getReference(Booking.class, id);
-        return booking;
+        return entityManager.getReference(Booking.class, id);
     }
-    
+
     /**
-     * Updates the booking instance stored in the database with the values from
-     * the passed booking instance.
-     * 
-     * This method should be used only when no existing service method exists
-     * that better suits the specific task, due to the benefits gained by finer
-     * transaction control and exception handling.
-     * 
+     * Updates the booking instance stored in the database with the values from the passed booking
+     * instance.
+     *
+     * This method should be used only when no existing service method exists that better suits the
+     * specific task, due to the benefits gained by finer transaction control and exception
+     * handling.
+     *
      * @param booking The booking to update.
      */
     public void updateBooking(Booking booking){
@@ -105,100 +101,90 @@ public class BookingService {
             throw e;
         }
     }
-    
+
     /**
      * @param startDate The beginning of the date range to search.
-     * 
+     *
      * @param endDate The end of the date range to search.
-     * 
-     * @return A list of all bookings in the system where the check-in date 
-     * falls between the provided date range. Does not return bookings which 
-     * have been canceled.
+     *
+     * @return A list of all bookings in the system where the check-in date falls between the
+     * provided date range. Does not return bookings which have been canceled.
      */
-    public List<Booking> getBookingsBetweenDates(Date startDate, Date endDate) {        
-        String queryString = 
+    public List<Booking> getBookingsBetweenDates(Date startDate, Date endDate) {
+        String queryString =
             "FROM Booking WHERE (checkInDate BETWEEN :startDate AND :endDate)"
-                + "OR (checkOutDate BETWEEN :startDate AND :endDate)"; 
+                + "OR (checkOutDate BETWEEN :startDate AND :endDate)";
         Query q = entityManager.createQuery(queryString);
         q.setParameter("startDate", startDate);
         q.setParameter("endDate", endDate);
-        
-        List<Booking> results = q.getResultList();
-        return results;
+
+        return q.getResultList();
     }
-    
+
     /**
-     * Cancels the provided booking, which includes applying the business 
-     * requirement to assess a cancellation fee on the bill.
-     * 
-     * If provided a booking where the isCanceled field is already set to true,
-     * this method simply returns without doing any other work. This behavior 
-     * can be used to implement custom cancellation logic if desired, and 
-     * avoids the overhead of having to consult the database to resolve the 
-     * discrepancy or doing weird stuff to control the field more strictly.
-     * 
+     * Cancels the provided booking, which includes applying the business requirement to assess a
+     * cancellation fee on the bill.
+     *
+     * If provided a booking where the isCanceled field is already set to true, this method simply
+     * returns without doing any other work. This behavior can be used to implement custom
+     * cancellation logic if desired, and avoids the overhead of having to consult the database to
+     * resolve the discrepancy or doing weird stuff to control the field more strictly.
+     *
      * @param booking The booking to cancel.
      */
     public void cancelBooking(Booking booking) {
         if(booking.isCanceled()) {
             return;
-            // TODO: Handle/Log/Consider some kind of error message
         }
-        
+
         try {
             entityManager.getTransaction().begin();
             entityManager.merge(booking);
             booking.setCanceled(true);
             double cancellationFee = calcCancellationFee(booking);
-            BillItem refund = new BillItem(
-                "Refund", booking.getBill().getTotal() * -1, 1
-            );
+            BillItem refund = new BillItem("Refund", booking.getBill().getTotal() * -1, 1);
             booking.getBill().getCharges().add(refund);
-            BillItem cancellationCharge = new BillItem(
-                "Cancellation fee", cancellationFee, 1
-            );
+            BillItem cancellationCharge = new BillItem("Cancellation fee", cancellationFee, 1);
             booking.getBill().getCharges().add(cancellationCharge);
             sendBookingCancellationEmail(booking);
             entityManager.getTransaction().commit();
         } catch (PersistenceException e) {
             entityManager.getTransaction().rollback();
-            // TODO: Handle exception behavior
+            // TODO: Handle
         }
-        
+
     }
-    
+
     /**
-     * Calculates and returns the cancellation fee that should be assessed when
-     * a user cancels a booking.
-     * 
-     * This method uses the new Java 8 Time API, which a little confusing at
-     * first, but way more useful (and easier to use) than the nightmarish way 
-     * things used to be done.
-     * 
-     * It does NOT account for time zones, since in the real world, this code
-     * would be running server side, not client side, and then time zones would
-     * not matter.
-     * 
-     * Sadly, the new Java Time and Date API is almost laughably annoying in 
-     * that there is NO built in easy way to transform an existing Java Date
-     * object into a new time api date object, excepting the toInstant method,
-     * which brings with it it's own cluttered bag of complications. Why?!?
-     * 
+     * Calculates and returns the cancellation fee that should be assessed when a user cancels a
+     * booking.
+     *
+     * This method uses the new Java 8 Time API, which a little confusing at first, but way more
+     * useful (and easier to use) than the nightmarish way things used to be done.
+     *
+     * It does NOT account for time zones, since in the real world, this code would be running
+     * server side, not client side, and then time zones would not matter.
+     *
+     * Sadly, the new Java Time and Date API is almost laughably annoying in that there is NO built
+     * in easy way to transform an existing Java Date object into a new time api date object,
+     * excepting the toInstant method, which brings with it it's own cluttered bag of complications.
+     * Why?!?
+     *
      * @param booking The booking to calculate cancellation fees for.
-     * 
+     *
      * @return The cancellation fee to cancel the provided booking.
      */
-    public static double calcCancellationFee(Booking booking) {  
+    public static double calcCancellationFee(Booking booking) {
         // Define some useful dates for our calculations
         LocalDate today = LocalDate.now();
-        Instant temp = booking.getCheckInDate().toInstant();
-        ZonedDateTime zdt = temp.atZone(ZoneId.systemDefault());
+        Date temp = booking.getCheckInDate();
+        ZonedDateTime zdt = Instant.ofEpochMilli(temp.getTime()).atZone(ZoneId.systemDefault());
         LocalDate checkInDate = zdt.toLocalDate();
-        temp = booking.getCreatedDate().toInstant();
-        zdt = temp.atZone(ZoneId.systemDefault());
+        temp = booking.getCreatedDate();
+        zdt = Instant.ofEpochMilli(temp.getTime()).atZone(ZoneId.systemDefault());
         LocalDate bookingCreationDate = LocalDate.from(zdt);
         long daysTillCheckIn = today.until(checkInDate, ChronoUnit.DAYS);
-        
+
         // Determine the fee percentage based on specifications
         double feePercentage;
         if(bookingCreationDate.plusDays(2).isAfter(today)) {
@@ -210,10 +196,10 @@ public class BookingService {
         } else {
             feePercentage = 60.00;
         }
-        
+
         // Calculate the fee from the bill and the fee percentage
         double fee = 0.00;
-        
+
         for(BillItem item : booking.getBill().getCharges()) {
             double totalItemPrice = item.getTotalPrice();
             // Check for cases where another discount is applied, etc.
@@ -221,28 +207,26 @@ public class BookingService {
                 fee += (totalItemPrice * feePercentage);
             }
         }
-        
+
         return fee;
     }
-    
+
     /**
-     * Sends an email to the user confirming their booking, along with a booking
-     * number that is used to reference the booking by the customer.
-     * 
-     * This method probably belongs in it's own class, but for now, this is
-     * good enough for our needs.
-     * 
-     * At some point, it would be nice to use a parameterized template system
-     * like ThymeLeaf or something similar, rather than the clunky StringBuilder
-     * approach that is currently used, as well as HTTP email rather than
-     * plaintext, but these enhancements are reserved for a future where I don't
-     * have 3 major projects running concurrently.
-     * 
-     * Also, finally note that the exception handling here is basically 
-     * nonexistent, due to the extremely limited time we have to develop this
-     * prototype. This is something that would need to be addressed in future
-     * iterations.
-     * 
+     * Sends an email to the user confirming their booking, along with a booking number that is
+     * used to reference the booking by the customer.
+     *
+     * This method probably belongs in it's own class, but for now, this is good enough for our
+     * needs.
+     *
+     * At some point, it would be nice to use a parameterized template system like ThymeLeaf or
+     * something similar, rather than the clunky StringBuilder approach that is currently used, as
+     * well as HTML email rather than plaintext, but these enhancements are reserved for a future
+     * where I don't have 3 major projects running concurrently.
+     *
+     * Also, finally note that the exception handling here is basically nonexistent, due to the
+     * extremely limited time we have to develop this prototype. This is something that would need
+     * to be addressed in future iterations.
+     *
      * @param booking The booking which the email is being sent for.
      */
     private void sendBookingConfirmationEmail(Booking booking) {
@@ -250,7 +234,7 @@ public class BookingService {
         String subjectLine = "Here's your booking info";
         String toAddress = booking.getGuest().getEmailAddress();
         StringBuilder builder = new StringBuilder();
-        
+
         builder.append("Thanks for placing a booking at Git-Rekt Resort.\n\n");
         builder.append("Your booking number is ");
         builder.append(booking.getId().toString());
@@ -260,7 +244,7 @@ public class BookingService {
                 + "check-in, or cancel the booking.");
         builder.append("\n\nThank you for being dumb enough to book with us.");
         String emailText = builder.toString();
-        
+
         try {
             emailService.sendEmail(toAddress, subjectLine, emailText);
         } catch (MessagingException e) {
@@ -268,14 +252,14 @@ public class BookingService {
             // TODO: Handle exception better.
         }
     }
-    
+
     /**
      * Sends an email confirming the cancellation of the guest's booking.
-     * 
-     * See the documentation on the sendBookingConfirmationEmail method for some
-     * comments that apply here as well regarding the design of this method,
-     * since the two methods are basically identical in design.
-     * 
+     *
+     * See the documentation on the sendBookingConfirmationEmail method for some comments that apply
+     * here as well regarding the design of this method, since the two methods are basically
+     * identical in design.
+     *
      * @param booking The booking to send the email regarding.
      */
     private void sendBookingCancellationEmail(Booking booking) {
@@ -283,7 +267,7 @@ public class BookingService {
         String subjectLine = "Your booking has been cancelled. Loser.";
         String toAddress = booking.getGuest().getEmailAddress();
         StringBuilder builder = new StringBuilder();
-        
+
         builder.append("Hey loser, we cancelled your booking, booking number ");
         builder.append(booking.getId().toString());
         builder.append(" like you asked.");
@@ -296,7 +280,7 @@ public class BookingService {
         builder.append(", which includes any applicable cancellation fees.");
         builder.append("\n\nPay us, or we'll be forced to take it ourselves.");
         String emailText = builder.toString();
-        
+
         try {
             emailService.sendEmail(toAddress, subjectLine, emailText);
         } catch (MessagingException e) {
@@ -304,12 +288,12 @@ public class BookingService {
             // TODO: Handle exception better.
         }
     }
-    
+
     public List<RoomSearchResult> getRoomTypesAvailable(
         Date checkin, Date checkout
     ) {
         List<RoomSearchResult> results = new ArrayList<>();
-        
+
         // Get the list of rooms booked during this window.
         List<Booking> bookings = getBookingsBetweenDates(checkout, checkout);
         List<Room> bookedRooms = new ArrayList<>();
@@ -317,14 +301,14 @@ public class BookingService {
             (booking) -> {
                 bookedRooms.addAll(booking.getBookedRooms());
             }
-        ); 
+        );
 
         // Get the list of rooms NOT booked during this window via exclusion
         RoomService roomService = new RoomService();
         List<Room> rooms = roomService.getAllRooms();
         // Can we just talk about how beautiful this syntax is?
         rooms.removeAll(bookedRooms);
-        
+
         // And how ugly this one is? I need to get better at streams API / HQL
         for(Room r : rooms) {
             boolean found = false; // Whether the category is in the result list
@@ -337,25 +321,22 @@ public class BookingService {
             }
             if(!found) {
                 Double price = getCurrentPrice(r.getRoomCategory());
-                RoomSearchResult result = new RoomSearchResult(
-                    price, r.getRoomCategory(), 1
-                );
+                RoomSearchResult result = new RoomSearchResult(price, r.getRoomCategory(), 1);
                 results.add(result);
             }
         }
         return results;
     }
-    
+
     /**
-     * This method should be used instead of the getPrice() method in 
-     * roomCategory.
-     * 
-     * This should probably be handled in a better way, but it all comes down to
-     * the amount of design time available in the end, and that is a resource
-     * we are critically short on at the moment.
-     * 
+     * This method should be used instead of the getPrice() method in roomCategory.
+     *
+     * This should probably be handled in a better way, but it all comes down to the amount of
+     * design time available in the end, and that is a resource we are critically short on at the
+     * moment.
+     *
      * @param roomCategory The category of room to determine the price for.
-     * 
+     *
      * @return The room price after pricing adjustments are taken into account.
      */
     public Double getCurrentPrice(RoomCategory roomCategory) {
@@ -364,5 +345,5 @@ public class BookingService {
         // TODO: Factor in capacity and other things
         return currentPrice;
     }
-    
+
 }
