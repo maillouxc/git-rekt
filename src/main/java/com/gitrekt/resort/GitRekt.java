f9b8d1901da9702fb603ddb42f3911f54da8bc1c
Chris Mailloux <mailloux.cl@gmail.com>
2017-11-21 20:54:01 -0500

    Fix dialog sizes introduced by master CSS

--- src/main/java/com/gitrekt/resort/GitRekt.java
+++ src/main/java/com/gitrekt/resort/GitRekt.java
@@ -3,22 +3,9 @@ package com.gitrekt.resort;
 
 import com.gitrekt.resort.controller.ScreenManager;
 import com.gitrekt.resort.hibernate.HibernateUtil;
-import com.gitrekt.resort.model.entities.Bill;
-import com.gitrekt.resort.model.entities.Booking;
-import com.gitrekt.resort.model.entities.Guest;
-import com.gitrekt.resort.model.entities.Package;
-import com.gitrekt.resort.model.entities.Room;
-import com.gitrekt.resort.model.services.BookingService;
 import com.gitrekt.resort.model.services.GuestService;
-import com.gitrekt.resort.model.services.PackageService;
-import com.gitrekt.resort.model.services.RoomService;
 import java.awt.print.PrinterException;
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.TimeZone;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -54,8 +41,6 @@ public class GitRekt extends Application {
 
         DatabaseTestDataLoader.initializeTestData();
 
-        // TODO: Remove
-        testRandomStuffRealQuick();
     }
 
     /**
@@ -68,18 +53,4 @@ public class GitRekt extends Application {
         HibernateUtil.close();
     }
 
-    // TODO: Remove ASAP
-    private void testRandomStuffRealQuick() {
-        BookingService bookingService = new BookingService();
-        PackageService packageService = new PackageService();
-        GuestService guestService = new GuestService();
-        RoomService roomService = new RoomService();
-        List<Room> rooms = roomService.getAllRoomsInCategory("Basic");
-        List<Package> packages = packageService.getAllPackages();
-        Guest g = guestService.getGuestById(1L);
-        Calendar c = new GregorianCalendar(TimeZone.getDefault());
-        c.add(Calendar.DATE, 1);
-        Booking b = new Booking(g, new Date(), c.getTime(), new Bill(), "", packages, rooms);
-        bookingService.createBooking(b);
-    }
 }
