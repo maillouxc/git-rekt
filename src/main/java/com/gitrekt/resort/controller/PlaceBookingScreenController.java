6c18c611e3ccec0bb531949536b75ed5583c47ad
Chris Mailloux <mailloux.cl@gmail.com>
2017-11-22 10:00:37 -0500

    Minor Javadoc updates

--- src/main/java/com/gitrekt/resort/controller/PlaceBookingScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/PlaceBookingScreenController.java
@@ -15,8 +15,8 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 /**
- * FXML Controller class for the screen where the user enters their name and
- * other relevant info for their booking, such as billing address.
+ * FXML Controller class for the screen where the user enters their name and other relevant info
+ * for their booking, such as billing address.
  */
 public class PlaceBookingScreenController implements Initializable {
 
@@ -60,15 +60,15 @@ public class PlaceBookingScreenController implements Initializable {
 
     private ObservableList<String> states;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initializeStatePicker();
         initializeCountryPicker();
     }
 
+    /**
+     * Returns to the Browse Rooms screen.
+     */
     @FXML
     private void onBackButtonClicked() {
         ScreenManager.getInstance().switchToScreen("/fxml/BrowseRoomsScreen.fxml");
@@ -105,7 +105,11 @@ public class PlaceBookingScreenController implements Initializable {
         }
     }
 
-    public void initializeCountryPicker() {
+    /**
+     * Initializes the country picker with a list of all valid world countries, as obtained by the
+     * Java locale class.
+     */
+    private void initializeCountryPicker() {
         countries = FXCollections.observableArrayList();
         Locale[] locales = Locale.getAvailableLocales();
         for (Locale locale : locales) {
@@ -118,7 +122,14 @@ public class PlaceBookingScreenController implements Initializable {
         countryPicker.setItems(countries);
     }
 
-    public void onCountryPicked() {
+    /**
+     * Called whenever a country is selected in the country picker.
+     *
+     * Checks if the selected country is the United States, and updates the visibility of the state
+     * picker accordingly.
+     */
+    @FXML
+    private void onCountryPicked() {
         if(countryPicker.getValue().equals("United States")) {
             statePicker.setVisible(true);
             statePickerLabel.setVisible(true);
@@ -128,7 +139,13 @@ public class PlaceBookingScreenController implements Initializable {
         }
     }
 
-    public void initializeStatePicker() {
+    /**
+     * Initializes the state picker with all US states plus Puerto Rico as choices.
+     *
+     * The state picker is set to be hidden initially. The program should unhide the state picker
+     * when it is determined that the user is in the United States.
+     */
+    private void initializeStatePicker() {
         states = FXCollections.observableArrayList();
         for(UsState state : UsState.values()) {
             states.add(state.getUnabbreviated());
@@ -139,7 +156,28 @@ public class PlaceBookingScreenController implements Initializable {
     }
 
     private boolean validateInput() {
-        return true;
-        // TODO
+        boolean isValid = true;
+
+        // Gather all input from all fields
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String email = emailField.getText();
+        String confirmedEmail = confirmEmailField.getText();
+        String addressLine1 = addressLine1Field.getText();
+        String addressLine2 = addressLine2Field.getText();
+        String city = cityField.getText();
+        String country = countryPicker.getValue();
+        String postalCode = postalCodeField.getText();
+        String specialInstructions = specialInstructionsBox.getText();
+
+        if(!confirmedEmail.equals(email)) {
+            isValid = false;
+        }
+
+
+        return isValid;
+        // TODO Finish validation
+        // TODO UI cues
+        // TODO trim input
     }
 }
