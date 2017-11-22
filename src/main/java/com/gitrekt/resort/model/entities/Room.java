0cf2c562501c2b3ebf3349b5e55769191f8ef4ec
Chris Mailloux <mailloux.cl@gmail.com>
2017-11-22 13:07:37 -0500

    Implement hashCode and equals for Room
    This fixes the bug where rooms that should have been considered booked
    were not.

--- src/main/java/com/gitrekt/resort/model/entities/Room.java
+++ src/main/java/com/gitrekt/resort/model/entities/Room.java
@@ -1,5 +1,6 @@
 package com.gitrekt.resort.model.entities;
 
+import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,13 +8,13 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Room {
-    
+
     @Id
     private String roomNumber;
-    
+
     @ManyToOne(cascade = CascadeType.ALL)
     private RoomCategory roomCategory;
-    
+
     /**
      * DO NOT CALL THIS CONSTRUCTOR. IT EXISTS ONLY BECAUSE IT IS REQUIRED BY
      * HIBERNATE.
@@ -21,18 +22,47 @@ public class Room {
     Room() {
         // REQUIRED BY HIBERNATE
     }
-    
+
     public Room(String roomNumber, RoomCategory roomCategory) {
         this.roomNumber = roomNumber;
         this.roomCategory = roomCategory;
     }
-    
+
     public String getRoomNumber() {
         return roomNumber;
     }
-    
+
     public RoomCategory getRoomCategory() {
         return roomCategory;
     }
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.roomNumber);
+        hash = 17 * hash + Objects.hashCode(this.roomCategory);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null) {
+            return false;
+        }
+        if(getClass() != obj.getClass()) {
+            return false;
+        }
+        final Room other = (Room) obj;
+        if(!Objects.equals(this.roomNumber, other.roomNumber)) {
+            return false;
+        }
+        if(!Objects.equals(this.roomCategory, other.roomCategory)) {
+            return false;
+        }
+        return true;
+    }
+
 }
