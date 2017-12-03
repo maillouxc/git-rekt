803dcfe72395f4bd07eb9af6aca92eb22e7923ac
Chris Mailloux <mailloux.cl@gmail.com>
2017-12-03 12:22:22 -0500

    Yet another refactoring

--- src/main/java/com/gitrekt/resort/controller/EditPricesScreenController.java
+++ src/main/java/com/gitrekt/resort/controller/EditPricesScreenController.java
@@ -1,8 +1,7 @@
 package com.gitrekt.resort.controller;
 
-import com.gitrekt.resort.model.services.RoomService;
-import com.gitrekt.resort.model.entities.Room;
 import com.gitrekt.resort.model.entities.RoomCategory;
+import com.gitrekt.resort.model.services.RoomService;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -13,24 +12,31 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+/**
+ * The FXML controller class for the edit prices screen.
+ */
 public class EditPricesScreenController implements Initializable {
 
     @FXML
-    private Button backButton;
-    @FXML
-    private Button editPriceButton;
-    @FXML
     private TableView<RoomCategory> roomTableView;
+
     @FXML
     private TableColumn<RoomCategory,String> roomNameColumn;
+
     @FXML
     private TableColumn<RoomCategory,String> roomDescriptionColumn;
+
     @FXML
     private TableColumn<RoomCategory,Double> roomPriceColumn;
 
     private ObservableList<RoomCategory> room;
 
-
+    /**
+     * Initializes the FXML controller class.
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         room = FXCollections.observableArrayList();
@@ -52,30 +58,34 @@ public class EditPricesScreenController implements Initializable {
             return new SimpleDoubleProperty(
                     param.getValue().getBasePrice()
             ).asObject();
-        } );
+        });
 
         loadData();
-
     }
 
+    /**
+     * Pops up a dialog that prompts the user to change the price.
+     */
     @FXML
-    private void oneditPriceClickedButton() {
-
+    private void onEditPriceClickedButton() {
+        // TODO
     }
 
+    /**
+     * Returns to the staff home screen.
+     */
     @FXML
     private void onBackButtonClicked() {
         ScreenManager.getInstance().switchToScreen("/fxml/StaffHomeScreen.fxml");
     }
 
+    /**
+     * Loads the pricing data for the resort.
+     */
     private void loadData() {
         RoomService roomService = new RoomService();
         room.addAll(roomService.getAllRoomCategories());
     }
-
-
-
-
 
 }
 
